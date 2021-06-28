9343d64981f5918a071efbf53ace3381ad88eee5
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-06-28 08:58:22 -0300

    Aula sobre comentários

---
+++ Aula 04/index.js
@@ -0,0 +1,12 @@
+//Aula sobre comentários em códiso em JavaScript
+console.log('Teste 01');
+
+/*
+console.log('Teste 02');
+console.log('Teste 03');
+console.log('Teste 04');
+console.log('Teste 05');
+console.log('Teste 06');
+*/
+
+console.log('Teste 07');
