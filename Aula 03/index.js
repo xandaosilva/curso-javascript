41b0f84d238241dd48f7e1f2fb2b6a598353b2c5
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-06-28 08:44:46 -0300

    Exercício Console log

--- Aula 03/index.js
+++ Aula 03/index.js
@@ -1,5 +1,8 @@
+//Testes
 console.log("Teste '01'");
 console.log('Teste "02"');
 console.log(`'Teste' "03"`);
-
 console.log(12, 15.85, "Teste 04");
+
+//Exercício
+console.log('Meu nome é "Alexandre". Estou aprendendo JavaScripti às',8,'da manhã.')
