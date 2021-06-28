131e2baeffee88d030c8e5fc11931851d1fccd69
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-06-28 08:35:41 -0300

    Console Log

---
+++ Aula 02/index.js
@@ -0,0 +1,1 @@
+console.log("Aula configuraç~çoes em Linux");
---
+++ Aula 03/index.js
@@ -0,0 +1,5 @@
+console.log("Teste '01'");
+console.log('Teste "02"');
+console.log(`'Teste' "03"`);
+
+console.log(12, 15.85, "Teste 04");
