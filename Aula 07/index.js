a298eae177adf2b561305a8c9b868fd5d1da747d
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-06-29 07:42:24 -0300

    Const

---
+++ Aula 07/index.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+const soma = primeiroNumero + segundoNumero;
+const subtracao = primeiroNumero - segundoNumero;
+const multiplicacao = primeiroNumero * segundoNumero;
+const multiplicacaoDuplicada = multiplicacao * 2;
+const divisao = primeiroNumero/segundoNumero;
+
+console.log(soma, subtracao, multiplicacao, multiplicacaoDuplicada, divisao);
