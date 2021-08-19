7aba0a0a2c08f3837c40f8ece82e5b5be11dc28b
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-08-19 09:49:32 -0300

    Tipos de dados primitivos

---
+++ Aula 09/index.js
@@ -0,0 +1,22 @@
+//sting
+const nome1 = 'teste';
+const nome2 = "teste";
+const nome3 = `teste`;
+
+//number
+const num1 = 10;
+const num2 = 10.25;
+
+//undefined e null, onde a variável não aponta para alguma lugar na memória
+let nomeAluno;
+const sobrenomeAluno = null;
+
+//boolean
+const aprovado = false;
+const reprovado = true;
+
+console.log(nome1, nome2, nome3);
+console.log(num1, num2);
+console.log(nomeAluno, sobrenomeAluno);
+console.log(aprovado);
+console.log(reprovado);
