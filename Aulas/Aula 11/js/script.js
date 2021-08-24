8192d18eeb6b6456ee1e228e954666fd28c1bb1b
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-08-24 01:54:29 -0300

    Alert, confirm, prompt e exercício

---
+++ Aulas/Aula 11/js/script.js
@@ -0,0 +1,3 @@
+let num1 = parseFloat(prompt("Digite o primeiro número :"));
+let num2 = parseFloat(prompt("Digite o segundo número :"));
+alert(`O resultado da operação: ${num1} + ${num2} = ${num1 + num2}`);
