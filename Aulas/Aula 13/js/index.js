67287f08b639af66b166dc9fb6f9dd687a45e312
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-08-25 18:27:06 -0300

    Aula e exercício sobre Strings

---
+++ Aulas/Aula 13/js/index.js
@@ -0,0 +1,11 @@
+let msg01 = 'Mensagem de "TESTE"';
+let msg02 = "Mensagem de \"TESTE\"";
+let msg03 = "Mensagem de \\TESTE";
+let msg04 = "Um texto";
+
+console.log(msg01);
+console.log(msg02);
+console.log(msg03);
+console.log(msg04[4]);
+console.log(msg04.charAt(3));
+console.log(`${msg04} para ser lido.`);
