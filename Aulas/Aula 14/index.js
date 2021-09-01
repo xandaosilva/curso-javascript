eb6cafee18f99c5b9aa54aeca16c520990d20a82
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-01 01:05:15 -0300

    Mais sobre numbers

---
+++ Aulas/Aula 14/index.js
@@ -0,0 +1,14 @@
+let num1 = 1500;
+let num2 = 2.5;
+let num3 = 10.589754657897;
+let num4 = 0.7;
+let num5 = 0.1;
+
+num4 = (((num4 * 100) + (num5 * 100)) / 100);
+
+console.log(num1.toString(2)); //conversão para binário
+console.log(num3.toFixed(2));
+console.log(Number.isInteger(num1));
+console.log(Number.isInteger(num2));
+console.log(num4);
+console.log(Number.isInteger(num4));
