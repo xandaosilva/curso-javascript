b5e3777d8d79c89aa4eb03cf748bfbdb7c7c7b23
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-01 02:07:50 -0300

    Objeto Math

---
+++ Aulas/Aula 15/index.js
@@ -0,0 +1,16 @@
+let num1 = 9.54578;
+
+//Math.random(); número aleatório enter 0 e 1
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
+console.log(Math.floor(num1));
+console.log(Math.ceil(num1));
+console.log(Math.round(num1));
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+console.log(aleatorio); 
+console.log(Math.PI);
+console.log(Math.pow(3,2));
+console.log(Math.pow(9,0.5));
+console.log(Math.sqrt(100));
+console.log(100/0);
