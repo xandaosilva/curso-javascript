259c3b08ae3ea6145f91d8235c3bbac4a35583d1
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-02 11:29:26 -0300

    Início do exercício sobre cálculo de IMC

---
+++ Aulas/Capitulo 03/Aula 26/assets/js/main.js
@@ -0,0 +1,14 @@
+/*
+< 18.5 abaixo do peso
+< 25 peso normal
+< 30 sobrepeso
+< 35 obesidade grau 1
+< 40 obesidade grau 2
+>= 40 obesidade grau 3
+
+validar campos
+validar dados
+mostrar mensagens
+calcular imc
+
+*/
