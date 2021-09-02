4fd4f4d839c6e3a2f5b6726405e3e8b89e111952
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-02 08:07:10 -0300

    Operadores de comparação

---
+++ Aulas/Aula 21/index.js
@@ -0,0 +1,29 @@
+/*
+OPERADORES COMPARATIVOS
+> MAIOR
+>= MAIOR OU IGUAL
+< MENOR
+<= MENOR OU IGUAL
+== IGUALDADE ( VALORES ) -> RECOMENDAÇÃO NÃO UTILIZAR
+=== IGUALDADE ESTRITA ( TIPOS E VALORES )
+!= DIFERENTE ( VALORES ) -> RECOMENDAÇÃO NÃO UTILIZAR
+!== DIFERENTE ESTRITO ( TIPOS E VALORES )
+*/
+
+const comp1 = 2 > 1;
+const comp2 = 2 >= 3;
+const comp3 = 22 < 47;
+const comp4 = 34 <= 35;
+const comp5 = "F" == "A";
+const comp6 = 3 === 3;
+const comp7 = "F" != "F";
+const comp8 = 3 !== "3";
+
+console.log(comp1);
+console.log(comp2);
+console.log(comp3);
+console.log(comp4);
+console.log(comp5);
+console.log(comp6);
+console.log(comp7);
+console.log(comp8);
