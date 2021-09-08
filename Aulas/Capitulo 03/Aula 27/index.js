108cae70dd8a19f61d45b990c33f92b7313c1142
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-08 14:59:28 -0300

    Operação ternária

---
+++ Aulas/Capitulo 03/Aula 27/index.js
@@ -0,0 +1,12 @@
+function verificarParOuImpar(numero){
+    numero % 2 === 0 ? console.log("Número par") : console.log("Número ímpar");
+}
+
+verificarParOuImpar(1);
+verificarParOuImpar(2);
+verificarParOuImpar(4);
+verificarParOuImpar(11);
+verificarParOuImpar(22);
+verificarParOuImpar(27);
+verificarParOuImpar(100);
+verificarParOuImpar(103);
