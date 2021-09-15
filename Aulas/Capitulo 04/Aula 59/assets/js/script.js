b003be63d0a7b914535db44d69fa9c4c34921973
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-15 09:22:39 -0300

    Correção classe Calculadora

--- Aulas/Capitulo 04/Aula 59/assets/js/script.js
+++ Aulas/Capitulo 04/Aula 59/assets/js/script.js
@@ -1,54 +1,54 @@
-function Calculadora(){
-    this.display = document.querySelector(".display");
+class Calculadora {
+    constructor() {
+        this.display = document.querySelector(".display");
 
-    this.iniciarCalculadora = () => {
-        this.capturarCliques();
-        this.calcularPorEnter();
-    };
+        this.iniciarCalculadora = () => {
+            this.capturarCliques();
+            this.calcularPorEnter();
+        };
 
-    this.alterarValorDisplay = (elemento) => {
-        this.display.value += elemento.innerText;
-        this.display.focus();
-    };
+        this.alterarValorDisplay = (elemento) => {
+            this.display.value += elemento.innerText;
+            this.display.focus();
+        };
 
-    this.limparDisplay = () => this.display.value = "";
-    
-    this.deletarCaractere = () => this.display.value = this.display.value.slice(0 , -1);
+        this.limparDisplay = () => this.display.value = "";
 
-    this.calcular = () => {
-        try {
-            let conta = eval(this.display.value);
+        this.deletarCaractere = () => this.display.value = this.display.value.slice(0, -1);
 
-            if(!conta){
+        this.calcular = () => {
+            try {
+                let conta = eval(this.display.value);
+
+                if (!conta) {
+                    alert("Operação inválida !");
+                    return;
+                }
+
+                this.display.value = conta;
+            } catch (error) {
                 alert("Operação inválida !");
                 return;
             }
+        };
 
-            this.display.value = conta;
-        } catch (error) {
-            alert("Operação inválida !");
-            return;
-        }
-    };
+        this.calcularPorEnter = () => {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) this.calcular();
+            });
+        };
 
-    this.calcularPorEnter = () => {
-        this.display.addEventListener('keyup', e => {
-            if (e.keyCode === 13) {
-              this.calcular();
-            }
-        });
-    };
+        this.capturarCliques = () => {
+            document.addEventListener("click", evento => {
+                const elemento = evento.target;
 
-    this.capturarCliques = () => {
-        document.addEventListener("click", evento => {
-            const elemento = evento.target;
-
-            if(elemento.classList.contains("btn-num")) this.alterarValorDisplay(elemento);
-            if(elemento.classList.contains("btn-clear")) this.limparDisplay();
-            if(elemento.classList.contains("btn-del")) this.deletarCaractere();
-            if(elemento.classList.contains("btn-eq")) this.calcular();
-        });
-    };
+                if (elemento.classList.contains("btn-num")) this.alterarValorDisplay(elemento);
+                if (elemento.classList.contains("btn-clear")) this.limparDisplay();
+                if (elemento.classList.contains("btn-del")) this.deletarCaractere();
+                if (elemento.classList.contains("btn-eq")) this.calcular();
+            });
+        };
+    }
 }
 
 const calculadora = new Calculadora();
