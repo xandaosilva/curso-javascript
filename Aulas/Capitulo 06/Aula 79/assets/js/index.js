9256d23c4d2eaa3e0485941f7af9a2f33d1ed76c
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-20 10:16:21 -0300

    Factory functions com prototypes

---
+++ Aulas/Capitulo 06/Aula 79/assets/js/index.js
@@ -0,0 +1,29 @@
+const falar = {
+    falar(){
+        console.log(`${this.nome} está falando.`);
+    },
+};
+
+const comer = {
+    comer(){
+        console.log(`${this.nome} está comendo.`);
+    },
+};
+
+const beber = {
+    beber(){
+        console.log(`${this.nome} está bebendo.`);
+    },
+};
+
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+function instanciarPessoa(nome, sobrenome){
+    return Object.create(pessoaPrototype, {
+        nome: { value:nome },
+        sobrenome: { value:sobrenome }
+    });
+}
+
+const pessoa = instanciarPessoa("Maria", "Eduarda");
+console.log(pessoa);
