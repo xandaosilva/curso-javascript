b86203dafa66c409291506e93799d0d6558e10b5
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-09-22 11:12:36 -0300

    Fetch API e axios

---
+++ Aulas/Capitulo 08/Aula 91/assets/js/main.js
@@ -0,0 +1,30 @@
+/*fetch("pessoas.json")
+    .then(resposta => resposta.json())
+    .then(json => carregarElementosNaPagina(json))
+    .catch(error => console.log(error));*/
+
+axios("pessoas.json").then(resposta => carregarElementosNaPagina(resposta.data));
+
+function carregarElementosNaPagina(json) {
+    const table = document.createElement("table");
+    for(let pessoa of json){
+        const tr = document.createElement("tr");
+        
+        let td = document.createElement("td");
+        td.innerHTML = pessoa.nome;
+        tr.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerHTML = pessoa.idade;
+        tr.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerHTML = pessoa.salario;
+        tr.appendChild(td);
+
+        table.appendChild(tr);
+    }
+
+    const resultado = document.querySelector(".resultado");
+    resultado.appendChild(table);
+}
