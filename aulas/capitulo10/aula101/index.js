7975c572ba655908f99dfafa3b90f74a37e65344
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-03-24 18:25:54 -0300

    Listagem de arquivos com recursão, leitura e escritura de arquivos

--- aulas/capitulo10/aula101/index.js
+++ aulas/capitulo10/aula101/index.js
@@ -1,18 +1,27 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-//fs.readdir(path.resolve(__dirname)).then(files => console.log(files)).catch(e => console.log(e));
-
 async function readdir(rootDir){
     rootDir = rootDir || path.resolve(__dirname);
     const files = await fs.readdir(rootDir);
-    walk(files);
+    walk(files, rootDir);
 }
 
-function walk(files){
+async function walk(files, rootDir){
     for(let file of files){
-        console.log(file);
+        const fileFullPath = path.resolve(rootDir, file);
+        const stats = await fs.stat(fileFullPath);
+
+        if(/\.git/g.test(fileFullPath)) continue;
+        if(/node_modules/g.test(fileFullPath)) continue;
+
+        if(stats.isDirectory()) {
+            readdir(fileFullPath);
+            continue;
+        }
+
+        console.log(fileFullPath, stats.isDirectory());
     }
 }
 
-readdir("c:/Users/xanda/Documents/CursoJS");
+readdir("C:/Users/xanda/Documents/Cursos/cursojs/curso-javascript");
